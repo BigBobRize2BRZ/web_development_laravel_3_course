cd42b2b987b108913f6bd93d23df0252f963ffe9
BigBobRize2BRZ <zarubin.pm07@gmail.com>
2026-03-25 10:52:41 +0500

    25.03.2026

---
+++ resources/views/user/dashboard.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+@section('title', $title ?? 'Test title')
+
+@section('content')
+    <h1 class="h2">Dashboard page</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,8 @@ use App\Http\Controllers\MainController;
 use App\Http\Controllers\TestController;
 use App\Http\Controllers\UserController;
 use App\Http\Middleware\VerifyCsrfToken;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -132,6 +134,23 @@ Route::post('posts', [PostController::class, 'store'])->name('posts.store');
 
 //! Верификация пользователя
 
+Route::get('dashboard', [UserController::class, 'dashboard'])->name('dashboard');
+
 Route::get('register', [UserController::class, 'create'])->name('register');
 Route::post('register', [UserController::class, 'store'])->name('user.store');
 Route::get('login', [UserController::class, 'login'])->name('login');
+
+Route::get('verify-email', function() {
+    return view('user.verify-email');
+})->middleware('auth')->name('verification.notice');
+
+Route::get('/email/verify/{id}/{hash}', function(EmailVerificationRequest $request) {
+    $request->fulfill();
+    return redirect()->route('dashboard');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+Route::get('/email/verification-notification', function(Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+
+    return back()->with('message', 'Verification link sent!');
+})->middleware(['auth', 'throttle:3.1'])->name('verification.send');
