0de1713dbda8587789db7f586defc8baa8decc1d
BigBobRize2BRZ <zarubin.pm07@gmail.com>
2026-04-01 13:06:29 +0500

    01.04.2026 (ошибка при сбросе пароля)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Auth\Events\PasswordReset;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Str;
 
 class UserController extends Controller
 {
@@ -44,4 +47,60 @@ class UserController extends Controller
     {
         return view('user.login', ['title' => 'Login']);
     }
+
+    public function loginAuth(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials, $request->boolean('remember'))) {
+            $request->session()->regenerate();
+            return redirect()->intended('dashboard')->with('success', 'Welcome ' . Auth::user()->name . '!');
+        }
+
+        return back()->withErrors([
+            'email' => 'Неккоректный логин и пароль',
+        ]);
+    }
+
+    public function forgotPasswordStore(Request $request)
+    {
+        $request->validate(['email' => 'required|email']);
+
+        $status = Password::sendResetLink(
+            $request->only('email')
+        );
+
+        return $status === Password::RESET_LINK_SENT 
+            ? back()->with(['success' => __($status)])
+            : back()->withErrors(['email' => __($status)]);
+    }
+
+    public function resetPasswordUpdate(Request $request)
+{
+    $request->validate([
+        'token' => 'required',
+        'email' => 'required|email',
+        'password' => 'required|min:6|confirmed',
+    ]);
+
+    $status = Password::reset(
+        $request->only('email', 'password', 'password_confirmation', 'token'),
+        function (User $user, string $password) {
+            $user->forceFill([
+                'password' => $password
+            ])->setRememberToken(Str::random(60));
+
+            $user->save();
+
+            event(new PasswordReset($user));
+        }
+    );
+
+    return $status === Password::PASSWORD_RESET
+        ? redirect()->route('login')->with(['success' => __($status)])
+        : back()->withErrors(['email' => __($status)]);
+}
 }
--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -3,5 +3,30 @@
 @section('title', $title ?? 'Test title')
 
 @section('content')
-    <h1 class="h2">Login form</h1>
+<h1 class="h2">Login form</h1>
+
+<form action="{{ route('login.auth') }}" method="post">
+    @csrf
+
+    <div class="mb-3">
+        <label for="email" class="form-label">Email</label>
+        <input name="email" type="email" class="form-control" id="email" placeholder="Email">
+    </div>
+
+    <div class="mb-3">
+        <label for="password" class="form-label">Password</label>
+        <input name="password" type="password" class="form-control" id="password" placeholder="Password">
+    </div>
+
+    <div class="mb-3 form-check">
+        <input name="remember" id="remember" class="form-check-input" type="checkbox">
+        <label class="form-check-label" for="remember">
+            Remember me
+        </label>
+    </div>
+
+    <button type="submit" class="btn btn-primary">Login</button>
+
+    <a href="{{ route('password.request') }}" class="ms-2">Forgot password?</a>
+</form>
 @endsection
